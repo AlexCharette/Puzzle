66a773f139a24f295cbcdcf85b9a70e5a3610d61
Alexander Charette <acharette.wake@gmail.com>
2016-12-11 09:35:30 -0500

    Refactored pause/play mechanic.

--- puzzle_ext/commandHandler.js
+++ puzzle_ext/commandHandler.js
@@ -1,8 +1,5 @@
 var oCommandHandler = function() {
-  this.sCurrentSystemCommand = "pause";
   this.asNodeCommands = [ "up", "down", "left", "right" ];
-  this.asSystemCommands = [ "run", "pause" ];
-  this.sError = "invalid";
   this.onKeyPress = function( ipKey ) {
     switch ( ipKey ) {
       case 'W' :
@@ -17,15 +14,7 @@ var oCommandHandler = function() {
       case 'D' :
         this.sCurrentNodeCommand = this.asNodeCommands[ 3 ];
       break;
-      case ' ' :
-        if ( this.sCurrentSystemCommand != this.asSystemCommands[ 0 ] ) {
-          this.sCurrentSystemCommand = this.asSystemCommands[ 0 ];
-        } else {
-          this.sCurrentSystemCommand = this.asSystemCommands[ 1 ];
-        }
-      break;
       default :
-        this.sCurrentNodeCommand = this.sError;
       break;
     }
   }
--- puzzle_ext/system.js
+++ puzzle_ext/system.js
@@ -1,6 +1,8 @@
 var oSystem = function() {
   this.oPath = new oPath();
   this.oSelectedNode = undefined;
+  this.fOpacity = 1.0;
+  this.bLevelIsFinished = false;
 
   this.init = function() {
     this.loadLevel( this.oCurrentLevel );
@@ -8,22 +10,35 @@ var oSystem = function() {
     this.checkPathProgress();
   }
 
-  this.setLevel = function( opLevel ) {
-    this.oCurrentLevel = opLevel;
-  }
-
   this.run = function() {
-    this.oCurrentLevel.oLayout.run();
     if ( this.oPath.bIsRunning ) {
       this.checkPathProgress();
     }
     this.oPath.run();
+    if ( !this.oPath.bIsRunning ) {
+      for ( oNode of this.aoNodes ) {
+        oNode.bWasCrossed = false;
+      }
+    }
+  }
+
+  this.render = function() {
+    if ( this.bLevelIsFinished ) {
+      this.fadeRender();
+    }
+    this.oCurrentLevel.oLayout.render( this.fOpacity );
+    this.oPath.render( this.fOpacity );
+    this.renderCursor( this.fOpacity );
   }
 
   this.loadPath = function() {
     var vFirstNodePos = this.aoNodes[ 0 ].oBody.vPosition;
     this.oPath.oBody.setStartPos( new oVector( vFirstNodePos.x, vFirstNodePos.y ) );
     this.oPath.init();
+  }
+
+  this.setLevel = function( opLevel ) {
+    this.oCurrentLevel = opLevel;
   }
 
   this.loadLevel = function( opLevel ) {
@@ -57,9 +72,32 @@ var oSystem = function() {
           this.oPath.oCurrentNode.bIsFirst = true;
         } else if ( this.aoNodes.indexOf( oNode ) == this.aoNodes.length - 1 ) {
           this.oPath.oCurrentNode.bIsLast = true;
-          this.bIsLevelFinished = true;
+          if ( this.oCurrentLevel.bRequiresKey
+               && !this.oPath.bFoundKey ) {
+            this.bLevelIsFinished = false;
+            this.oPath.bUserMadeMistake = true;
+            this.oPath.reset();
+          } else {
+            this.bLevelIsFinished = true;
+          }
         }
       }
     }
+  }
+
+  this.fadeRender = function() {
+    while ( this.fOpacity >= 0 ) {
+      this.fOpacity -= 0.01;
+    }
+  }
+
+  this.renderCursor = function() {
+    var fCursorSize = 20;
+    noStroke();
+    fill( 'rgba(67, 120, 181,' + this.fOpacity + ')' );
+    smooth();
+    ellipse(vMouse.x, vMouse.y, fCursorSize, fCursorSize);
+    fill('rgba(67, 120, 181,' + ( this.fOpacity / 3 ) + ')');
+    ellipse(vMouse.x, vMouse.y, fCursorSize * 1.5, fCursorSize * 1.5);
   }
 };
